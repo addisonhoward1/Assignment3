91c114d8b95b7aefd780aad638ab146274706aaa
addisonhoward1 <addison.howard@hotmail.com>
2021-02-03 19:00:11 -0700

    Initial check-in of module FilmCollection

---
+++ FilmCollection/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FilmCollection.Models;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult movieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult movieForm(ApplicationResponse appResponse)
+        {
+            TempStorage.Addapplication(appResponse);
+            return View("Confirmation", appResponse);
+        }
+
+        public IActionResult movieCollection()
+        {
+            return View(TempStorage.Applications.Where(r => r.Title != "Independence Day"));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FilmCollection/Models/ApplicationResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Models
+{
+    public class ApplicationResponse
+    {
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
